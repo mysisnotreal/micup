188a10c1e33a02120c1f4b5c3762e654b8b21b78
mys <bodhistarr.co@gmail.com>
2026-03-29 21:44:51 -0400

    Update source.lua

--- mys.client/Ui/source.lua
+++ mys.client/Ui/source.lua
@@ -769,7 +769,7 @@ local dragOffset = 255
 local dragOffsetMobile = 150
 
 Rayfield.DisplayOrder = 100
-LoadingFrame.Version.Text = Release
+LoadingFrame.Version.Text = "mys.client"
 
 -- Thanks to Latte Softworks for the Lucide integration for Roblox
 local Icons = useStudio and require(script.Parent.icons) or loadWithTimeout('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/refs/heads/main/icons.lua')
