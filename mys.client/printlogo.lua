2d6f48dd0f9b75e2f680f347b2eb15ec279348fc
mys <bodhistarr.co@gmail.com>
2026-04-07 09:35:23 -0400

    Create printlogo.lua

---
+++ mys.client/printlogo.lua
@@ -0,0 +1,7 @@
+local logo = game:HttpGet("https://raw.githubusercontent.com/mysisnotreal/micup/refs/heads/main/mys.client/logoprint.txt")
+
+if StartupPrint == true then
+    print(logo)
+else
+    warn("Startup Print Set To False.")
+end
